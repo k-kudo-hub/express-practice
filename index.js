cc4c42262fe99c3c8e4b0624d848e098a6e5bde0
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-15 08:50:13 +0900

    Get data from input value.

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 console.log('index.js: loaded');
 
+function main(){
+  fetchUserInfo('js-primer-example');
+}
+
 function fetchUserInfo(userId){
   fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then(response => {
-      console.log(response.status); // => 200
       if(!response.ok){
         console.log('エラーレスポンス', response);
       } else {
         return response.json().then(userInfo => {
           // HTMLの組み立て
-          const view = `
-          <h4>${userInfo.name} (@${userInfo.login})</h4>
-          <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-          <dl>
-            <dt>Location</dt>
-            <dd>${userInfo.location}</dd>
-            <dt>Repositories</dt>
-            <dd>${userInfo.public_repos}</dd>
-          </dl>
-          `;
+          const view = createView(userInfo);
           // HTMLの挿入
-          const result = document.getElementById('result');
-          result.innerHTML = view;
+          displayView(view);
         });
       }
     })
     .catch(error => {
       console.log(error);
     });
+}
+
+function createView(userInfo){
+  return escapeHTML`
+  <h4>${userInfo.name} (@${userInfo.login})</h4>
+  <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+  <dl>
+    <dt>Location</dt>
+    <dd>${userInfo.location}</dd>
+    <dt>Repositories</dt>
+    <dd>${userInfo.public_repos}</dd>
+  </dl>
+  `;
+}
+
+function displayView(view){
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
