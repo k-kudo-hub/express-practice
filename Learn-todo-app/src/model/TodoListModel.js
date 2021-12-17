7d6ded9ef798c93bbdad966e07a601921a0e927f
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-17 10:34:13 +0900

    Developed class of todo Item and todo list.

---
+++ Learn-todo-app/src/model/TodoListModel.js
@@ -0,0 +1,44 @@
+import { EventEmitter } from "../EventEmitter.js";
+
+export class TodoListModel extends EventEmitter {
+  constructor(items = []){
+    super();
+    this.items = items;
+  }
+
+  /**
+   * TodoItemの合計個数を返す
+   * @returns {number}
+   */
+  getTotalCount(){
+    return this.items.length
+  }
+
+  /**
+   * 表示できるTodoItemの配列を返す
+   * @returns {TodoItemModel[]}
+   */
+  getTodoItems(){
+    return this.items;
+  }
+
+  /**
+   * TodoListの状態が更新された時に呼び出されるリスナー関数を登録する
+   * @param {Function} listener 
+   */
+  onChange(listener) {
+    this.addEventListener("change", listener);
+  }
+
+  /**
+   * 状態が変更された時に呼ぶ。登録済みのリスナー関数を呼び出す
+   */
+  emitChange() {
+    this.emit("change")
+  }
+
+  addTodo(todoItem) {
+    this.items.push(todoItem);
+    this.emitChange();
+  }
+}
