fde097c390345a2ba46ae576cd717202cec7d550
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-17 10:43:45 +0900

    Function refactoring for adding Todo items.

--- Learn-todo-app/src/App.js
+++ Learn-todo-app/src/App.js
@@ -1,18 +1,40 @@
-import { element } from "./view/html-util.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { element, render } from "./view/html-util.js";
 
 export class App {
+  constructor() {
+    // 1. Construct TodoList.
+    this.todoListModel = new TodoListModel()
+  }
   mount() {
     const formElement = document.querySelector("#js-form");
     const inputElement = document.querySelector("#js-form-input");
     const containerElement = document.querySelector("#js-todo-list");
-    const todoItemCountElement = document.querySelector("#js-todo-count")
-    let todoItemCount = 0;
+    const todoItemCountElement = document.querySelector("#js-todo-count");
+    // 2. Update the display when the state of TodoListModel is updated.
+    this.todoListModel.onChange(() => {
+      // The list element wrapping todo list.
+      const todoListElement = element`<ul />`;
+      // Add each TodoItem element under todoListElement.
+      const todoItems = this.todoListModel.getTodoItems();
+      todoItems.forEach(item => {
+        const todoItemElement = element`<li>${item.title}</li>`;
+        todoListElement.appendChild(todoItemElement);
+      });
+      // Overwrite contents of containerElement by todoListElement.
+      render(todoListElement, containerElement);
+      // Update display of item counts.
+      todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+    })
+    // 3. After send form, add new TodoItemModel.
     formElement.addEventListener("submit", (e) => {
       e.preventDefault();
-      const todoItemElement = element`<li>${inputElement.value}</li>`;
-      containerElement.appendChild(todoItemElement);
-      todoItemCount += 1;
-      todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+      // Add new TodoItem for TodoList.
+      this.todoListModel.addTodo(new TodoItemModel({
+        title: inputElement.value,
+        completed: false
+      }));
       inputElement.value = "";
     });
   }
