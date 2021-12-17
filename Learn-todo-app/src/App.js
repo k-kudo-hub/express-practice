39a85849e72c49351d8a924b4dd6eaecdb3d1d0a
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-17 11:03:55 +0900

    Added the function to update the ToDo list.

--- Learn-todo-app/src/App.js
+++ Learn-todo-app/src/App.js
@@ -19,7 +19,17 @@ export class App {
       // Add each TodoItem element under todoListElement.
       const todoItems = this.todoListModel.getTodoItems();
       todoItems.forEach(item => {
-        const todoItemElement = element`<li>${item.title}</li>`;
+        // When todo is done, add a check. Otherwise, remove the check.
+        const todoItemElement = item.completed
+          ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></li>`
+          : element`<li><input type="checkbox" class="checkbox">${item.title}</li>`
+        const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+        inputCheckboxElement.addEventListener("change", () => {
+          this.todoListModel.updateTodo({
+            id: item.id,
+            completed: !item.completed
+          })
+        })
         todoListElement.appendChild(todoItemElement);
       });
       // Overwrite contents of containerElement by todoListElement.
