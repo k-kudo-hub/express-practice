568ba5d9fe69e5e5920c398d5eb5dd559fc8cfeb
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-18 16:49:11 +0900

    Start learning express.js.

---
+++ Learn-express/app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, () => {
+  console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+var imageList = [
+  {
+    id: "001",
+    name: "001.png",
+    type: "png",
+    dataUrl: "https://localhost:3000/src/images/001.png"
+  },
+  {
+    id: "002",
+    name: "002.png",
+    type: "png",
+    dataUrl: "https://localhost:3000/src/images/002.png"
+  }
+]
+
+app.get("/api/images/list", (req, res, next) => {
+  res.json(imageList);
+});
+
+app.get("/api/images/:imageId", (req, res, next) => {
+  const image = imageList.find(image => image.id === req.params.imageId);
+  if(!image){
+    return console.log("マッチする画像がありません。");
+  }
+  res.json(image);
+})
