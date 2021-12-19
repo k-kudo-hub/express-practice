8a2f618d5fa6345a2b19a19be4c304e23f3eb13c
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-19 17:37:23 +0900

    Variables related to DB are grouped into objects.

--- Learn-express/app.js
+++ Learn-express/app.js
@@ -1,21 +1,24 @@
-var express = require("express");
-var mysql   = require("mysql");
-var app = express();
+const express = require("express");
+const mysql   = require("mysql");
+const app = express();
 require('dotenv').config
 
-var server = app.listen(3000, () => {
+const server = app.listen(3000, () => {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
 
-const db_name = process.env.MYSQL_DATABASE;
-const db_pass = process.env.MYSQL_PASSWORD;
-const db_host = process.env.MYSQL_HOSTNAME;
-const db_user = process.env.MYSQL_USERNAME;
+const dbConfig = {
+  host: process.env.MYSQL_HOSTNAME,
+  name: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USERNAME, 
+  pass: process.env.MYSQL_PASSWORD,
+}
+
 const con = mysql.createConnection({
-  host: db_host,
-  user: db_user,
-  password: db_pass,
-  database: db_name
+  host: dbConfig.host,
+  database: dbConfig.name,
+  user: dbConfig.user,
+  password: dbConfig.pass,
 })
 
 con.connect((err) => {
@@ -23,7 +26,7 @@ con.connect((err) => {
   console.log('Mysql Connected');
 })
 
-var imageList = [
+const imageList = [
   {
     id: "001",
     name: "001.png",
