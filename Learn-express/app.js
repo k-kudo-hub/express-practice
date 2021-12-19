8fc3b3495a8af9e4d6fa58a7701f741590b4d0be
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-19 16:07:46 +0900

    Setting mysql connection using environment variable.

--- Learn-express/app.js
+++ Learn-express/app.js
@@ -1,9 +1,27 @@
 var express = require("express");
+var mysql   = require("mysql");
 var app = express();
+require('dotenv').config
 
 var server = app.listen(3000, () => {
   console.log("Node.js is listening to PORT:" + server.address().port);
 });
+
+const db_name = process.env.MYSQL_DATABASE;
+const db_pass = process.env.MYSQL_PASSWORD;
+const db_host = process.env.MYSQL_HOSTNAME;
+const db_user = process.env.MYSQL_USERNAME;
+const con = mysql.createConnection({
+  host: db_host,
+  user: db_user,
+  password: db_pass,
+  database: db_name
+})
+
+con.connect((err) => {
+  if(err) throw err;
+  console.log('Mysql Connected');
+})
 
 var imageList = [
   {
