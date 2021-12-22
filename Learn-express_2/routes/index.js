e3fbaf3f7d53a5e9ee0bec9d06dfd356a9026a8f
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-22 14:18:49 +0900

    Developed top page UI and data processing.

--- Learn-express_2/routes/index.js
+++ Learn-express_2/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express        = require('express');
+const router         = express.Router();
+const DB             = require("../public/javascripts/db.js");
+const { siteConfig, dbConfig } = require("../config/environment/config.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  (async () => {
+    const db    = new DB(dbConfig.host, dbConfig.name, dbConfig.user, dbConfig.pass);
+    const sql   = "SELECT * FROM `users` LIMIT 10";
+    const users = await db.query(sql);
+    res.render('index', {
+      title: 'Express',
+      siteName: siteConfig.title,
+      users: users
+    });
+  })().catch(next);
 });
 
 module.exports = router;
