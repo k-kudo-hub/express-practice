0aff0ef08466182cd30b8ee2d81bf4ad06eae39d
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-22 14:24:31 +0900

    Enabled to assign a value when issuing a query.

--- Learn-express_2/public/javascripts/db.js
+++ Learn-express_2/public/javascripts/db.js
@@ -35,11 +35,11 @@ class DB {
   // 2. asyncが値をreturnすると、Promiseは戻り値をresolveする。
   // 3. asyncが例外等をthrowすると、その値をrejectする。
 
-  async query(sql) {
+  async query(sql, value = []) {
     const data = await new Promise((resolve, reject) => {
       // このletは怪しい?非同期的に処理されるリスク?
       let con = this._set();
-      con.query(sql, (error, results, fields) => {
+      con.query(sql, value, (error, results, fields) => {
         // errorがあったらrejectする
         if(error) reject(error);
         // errorがなければresolveで値を運び、処理を継続
