6ac0c26ba40ef93cf270032f6e2d4073dfb4611a
k-kudo-hub <tongariisaibou@gmail.com>
2021-12-22 14:20:41 +0900

    Developed UI and data processing for user list page.

--- Learn-express_2/routes/users.js
+++ Learn-express_2/routes/users.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router  = express.Router();
+const DB    = require("../public/javascripts/db.js");
+const { dbConfig } = require("../config/environment/config.js");
+const db    = new DB(dbConfig.host, dbConfig.name, dbConfig.user, dbConfig.pass);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  (async () => {
+    const sql = "SELECT * FROM `users` LIMIT 10";
+    const users = await db.query(sql);
+    await console.log(users);
+    res.render('users/index', {
+      title: 'User',
+      users: users
+    });
+  })().catch(next);
+});
+
 });
 
 module.exports = router;
